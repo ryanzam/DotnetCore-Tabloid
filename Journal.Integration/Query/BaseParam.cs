ecb14fbcc7019ca44cec2eeba6408eee61ee6ecf
Ranjam al <ranjam.alee@ineo.fi>
2022-03-21 16:04:39 +0200

    add front end

---
+++ Journal.Integration/Query/BaseParam.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Journal.Model.Query
+{
+    public class BaseParam
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Journal.Web/Controllers/PostController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Journal.Model;
+using Journal.Model.Query;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Journal.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : ControllerBase
+    {
+        private readonly ILogger<PostController> _logger;
+
+        public PostController(ILogger<PostController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<PostModel>> GetAllPost()
+        {
+            var posts = new List<PostModel>();
+            using(var httpClient = new HttpClient())
+            {
+                using(var res = await httpClient.GetAsync("https://localhost:44395/api/Post"))
+                {
+                    string apiRes = await res.Content.ReadAsStringAsync();
+                    posts = JsonConvert.DeserializeObject<List<PostModel>>(apiRes);
+                }
+            }
+            return posts;
+        }
+    }
+}
