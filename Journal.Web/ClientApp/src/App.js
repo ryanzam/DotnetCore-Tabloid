b4f7963e40991cd7a43152a90e09b03c9a78696d
ryanzam <ranjam@ineo.fi>
2022-03-29 13:35:43 +0300

    user login

--- Journal.Web/ClientApp/src/App.js
+++ Journal.Web/ClientApp/src/App.js
@@ -1,25 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import CreateJournal from './components/CreateJournal';
 import Journal from './components/Journal'
-import Details from './components/Details';
-
+import Details from './components/Details'
+import Login from './components/Auth/Login'
+import useToken from './components/Auth/useToken';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
 
-  render () {
+function App() {
+    const { token, setToken } = useToken();
+
+    if (!token) {
+        return <>
+            <Login setToken={setToken} />
+            <div className="text-center">
+                <hr />
+                <p>Or</p>
+                <hr />
+                <button className="w-50 btn btn-lg btn-primary" type="submit">Register</button>
+            </div>
+        </>
+    }
+
     return (
-      <Layout>
+        <Layout>
         <Route exact path='/' component={Home} />
             <Route path='/journal' component={Journal} />
             <Route path='/details/:id' component={Details} />
             <Route path='/create' component={CreateJournal} />
-      </Layout>
+            <Route path='/signout' component={Login} />
+        </Layout>
     );
   }
-}
+
+export default App
---
+++ Journal.Web/ClientApp/src/components/Auth/Register.js
@@ -0,0 +1,67 @@
+﻿import React, { useEffect, useState } from 'react'
+//import Notification from '../Util/Notification'
+import { Journal } from 'react-bootstrap-icons'
+
+const Login = (props) => {
+
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        }
+
+        try {
+
+            const token = fetch('auth/login', requestOptions)
+                .then(data => data.json())
+
+            props.setToken(token)
+
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    const handlesUsrChange = event => {
+        setUsername(event.target.value)
+    }
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value)
+    }
+
+    const handlesPwdChange = event => {
+        setPassword(event.target.value)
+    }
+
+    return <div className="d-flex justify-content-center mt-5">
+        <form onSubmit={handleSubmit} className="text-center p-3">
+            <h1 className="display-5 text-white"><Journal /></h1>
+            <h1 className="h3 mb-3 fw-normal">Create new Account!</h1>
+
+            <div className="form-floating">
+                <input type="email" className="form-control" id="floatingUsr" placeholder="myusername" onChange={handlesUsrChange} />
+                <label for="floatingUsr">Username</label>
+            </div>
+
+            <div className="form-floating">
+                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" onChange={handleEmailChange} />
+                <label for="floatingInput">Email address</label>
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" onChange={handlesPwdChange} />
+                <label for="floatingPassword">Password</label>
+            </div>
+
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Register</button>
+        </form>
+    </div>
+}
+
+export default Login
